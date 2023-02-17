4b8c85715ce6600a050e61d537dcb7f801eb1c74
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-17 11:48:53 +0500

    new

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,47 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  // String s = 'Adham';
+  List l = [
+    'Adham',
+    'Bobur',
+    'Gayrat',
+    'Olimjon',
+    'Murod',
+    'Ozodbek',
+    'Elizavet',
+    'Hesreti var',
+    'Mirjalol',
+    "Ro'ziboy",
+    'Jurabek'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Empty List Test')),
+        appBar: AppBar(title: Center(child: Text('Empty List Test'))),
         body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: ((context, int index) {
-            return ListTile(
-              title: Text('Item${index + 1000589105901589259}'),
-            );
-          }),
-        ));
+            itemCount: l.length,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.yellow,
+                ),
+                margin: EdgeInsets.all(10),
+                height: 50,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(
+                      child: Text(index.toString()),
+                    ),
+                    Text(
+                      '    ${l[index]}',
+                      style: TextStyle(fontSize: 30),
+                    )
+                  ],
+                ),
+              );
+            }));
   }
 }
